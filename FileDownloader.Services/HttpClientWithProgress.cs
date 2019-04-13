81957369c37a658a2a19a9158ae3b04d164e755f
Spartak Bugdaryan <bugdaryan@gmail.com>
2019-04-14 01:17:23 +0400

    Add http client class with progress changed event

---
+++ FileDownloader.Services/HttpClientWithProgress.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FileDownloader.Services
+{
+    public delegate void ProgressChangedHandler(long? totalFileSize, long totalBytesDownloaded, double? progressPercentage);
+
+    class HttpClientWithProgress : IDisposable
+    {
+        private readonly string _downloadUrl;
+        private readonly string _destinationFilePath;
+
+        private HttpClient _httpClient;
+
+        public event ProgressChangedHandler ProgressChanged;
+
+        public void CancelPendingRequests()
+        {
+            _httpClient.CancelPendingRequests();
+        }
+
+        public HttpClientWithProgress(string downloadUrl, string destinationFilePath)
+        {
+            _downloadUrl = downloadUrl;
+            _destinationFilePath = destinationFilePath;
+        }
+
+        public async Task StartDownloadAsync()
+        {
+            _httpClient = new HttpClient { Timeout = TimeSpan.FromDays(1) };
+
+            using (var response = await _httpClient.GetAsync(_downloadUrl, HttpCompletionOption.ResponseHeadersRead))
+            {
+                await DownloadFileFromHttpResponseMessageAsync(response);
+            }
+        }
+
+        private async Task DownloadFileFromHttpResponseMessageAsync(HttpResponseMessage response)
+        {
+            response.EnsureSuccessStatusCode();
+
+            var totalBytes = response.Content.Headers.ContentLength;
+
+            using (var contentStream = await response.Content.ReadAsStreamAsync())
+            {
+                await ProcessContentStreamAsync(totalBytes, contentStream);
+            }
+        }
+
+        private async Task ProcessContentStreamAsync(long? totalDownloadSize, Stream contentStream)
+        {
+            var totalBytesRead = 0L;
+            var readCount = 0L;
+            var buffer = new byte[8192];
+            var isMoreToRead = true;
+
+            using (var fileStream = new FileStream(_destinationFilePath, FileMode.Create, FileAccess.Write, FileShare.None, 8192, true))
+            {
+                do
+                {
+                    var bytesRead = await contentStream.ReadAsync(buffer, 0, buffer.Length);
+                    if (bytesRead == 0)
+                    {
+                        isMoreToRead = false;
+                        TriggerProgressChanged(totalDownloadSize, totalBytesRead);
+                        continue;
+                    }
+
+                    await fileStream.WriteAsync(buffer, 0, bytesRead);
+
+                    totalBytesRead += bytesRead;
+                    readCount += 1;
+
+                    if (readCount % 10 == 0)
+                    {
+                        TriggerProgressChanged(totalDownloadSize, totalBytesRead);
+                    }
+                }
+                while (isMoreToRead);
+            }
+        }
+
+        private void TriggerProgressChanged(long? totalDownloadSize, long totalBytesRead)
+        {
+            if (ProgressChanged == null)
+            {
+                return;
+            }
+
+            double? progressPercentage = null;
+            if (totalDownloadSize.HasValue)
+            {
+                progressPercentage = Math.Round((double)totalBytesRead / totalDownloadSize.Value * 100, 2);
+            }
+
+            ProgressChanged(totalDownloadSize, totalBytesRead, progressPercentage);
+        }
+
+        public void Dispose()
+        {
+            _httpClient?.Dispose();
+        }
+    }
+}
