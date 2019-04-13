b33ac84031798cc581744de1947b8022a636ed2c
Spartak Bugdaryan <bugdaryan@gmail.com>
2019-04-13 04:41:04 +0400

    Add downloading functionality

--- FileDownloader.WPF/MainWindow.xaml.cs
+++ FileDownloader.WPF/MainWindow.xaml.cs
@@ -1,18 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+using System.Windows.Forms;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
+using Button = System.Windows.Controls.Button;
+using MessageBox = System.Windows.MessageBox;
+using ProgressBar = System.Windows.Controls.ProgressBar;
+using TextBox = System.Windows.Controls.TextBox;
 namespace FileDownloader.WPF
 {
     /// <summary>
@@ -20,9 +16,130 @@ namespace FileDownloader.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        Dictionary<Button, (TextBox, ProgressBar)> downloadControls;
+        int index = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+            downloadControls = new Dictionary<Button, (TextBox, ProgressBar)>();
+        }
+
+        private void AddNewDownloadButton_Click(object sender, RoutedEventArgs e)
+        {
+            var border = new Border
+            {
+                BorderBrush = Brushes.Black,
+                BorderThickness = new Thickness(2)
+            };
+
+            var newDownload = new StackPanel
+            {
+                Width = DownloadsListBox.Width * .95,
+                Height = 70,
+                Name = $"DownloadStackPanel_{index}"
+            };
+            border.Child = newDownload;
+
+            var textBox = new System.Windows.Controls.TextBox
+            {
+                Width = DownloadsListBox.Width * 0.9,
+                Height = 30,
+                Margin = new Thickness(5, 0, 0, 0),
+                Name = $"DownloadTextbox_{index}"
+            };
+            newDownload.Children.Add(textBox);
+
+            var button = new Button
+            {
+                Margin = new Thickness(10, 10, 10, 10),
+                Width = 100,
+                Height = 30,
+                HorizontalAlignment = System.Windows.HorizontalAlignment.Right,
+                Content = "Download",
+                Background = Brushes.DodgerBlue,
+                Foreground = Brushes.White,
+                Name = $"DownloadButton_{index}"
+            };
+            button.Click += Download_ClickAsync;
+            newDownload.Children.Add(button);
+
+            var progressBar = new System.Windows.Controls.ProgressBar
+            {
+                Name = $"DownloadProgressBar_{index}",
+                Width = 200,
+                Height = 30,
+                Margin = new Thickness(10)
+            };
+            progressBar.Visibility = Visibility.Collapsed;
+            newDownload.Children.Add(progressBar);
+
+            DownloadsListBox.Items.Add(border);
+            downloadControls.Add(button, (textBox, progressBar));
+            index++;
+        }
+
+        private async void Download_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            var button = ((Button)sender);
+            if (downloadControls.ContainsKey(button))
+            {
+                var textBox = downloadControls[button].Item1;
+
+                var progressBar = downloadControls[button].Item2;
+
+                int ind = textBox.Name[textBox.Name.Length - 1] - '0';
+                progressBar.Value = 0;
+
+                using (var dlg = new FolderBrowserDialog())
+                {
+                    var dialogResult = dlg.ShowDialog();
+
+                    if (dialogResult == System.Windows.Forms.DialogResult.OK)
+                    {
+                        var filePath = dlg.SelectedPath + $"\\Untitled_{ind}.txt";
+                        if (textBox.Text != string.Empty && Uri.IsWellFormedUriString(textBox.Text, UriKind.RelativeOrAbsolute))
+                        {
+                            using (var client = new WebClient())
+                            {
+                                client.DownloadProgressChanged += (send, args) =>
+                                {
+                                    progressBar.Value = args.ProgressPercentage;
+                                };
+
+                                progressBar.Visibility = Visibility.Visible;
+                                button.Visibility = Visibility.Collapsed;
+                                textBox.Visibility = Visibility.Collapsed;
+
+                                try
+                                {
+                                    await client.DownloadFileTaskAsync(textBox.Text, filePath);
+
+                                    MessageBox.Show($"Download {ind} completed!");
+                                }
+                                catch (WebException exception)
+                                {
+                                    MessageBox.Show(exception.Message);
+                                }
+                                catch (Exception)
+                                {
+                                    throw;
+                                }
+                                finally
+                                {                                    progressBar.Visibility = Visibility.Collapsed;
+                                    button.Visibility = Visibility.Visible;
+                                    textBox.Visibility = Visibility.Visible;
+                                }                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Invalid uri, please enter valid one");
+                        }
+                    }
+                }
+            }
+
         }
     }
 }
+
