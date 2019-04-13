6d39cfe4a0f31716a6abfbb4391ba3a714716503
Spartak Bugdaryan <bugdaryan@gmail.com>
2019-04-14 00:30:30 +0400

    Code refactoring Fix download bug Add project for services and helpers

---
+++ FileDownloader.Services/DownloadListBoxItem.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Media;
+using Button = System.Windows.Controls.Button;
+using MessageBox = System.Windows.MessageBox;
+using ProgressBar = System.Windows.Controls.ProgressBar;
+using TextBox = System.Windows.Controls.TextBox;
+
+
+namespace FileDownloader.Services
+{
+    public class DownloadListBoxItem
+    {
+        public Button DownloadButton { get; set; }
+        public Button CancelButton { get; set; }
+        public TextBox DownloadTextBox { get; set; }
+        public ProgressBar DownloadProgressBar { get; set; }
+        public CancellationTokenSource TokenSource { get; set; }
+        public Border DownloadBorder { get; set; }
+        public StackPanel DownloadPanel { get; set; }
+
+        public DownloadListBoxItem(int width)
+        {
+            DownloadBorder = new Border
+            {
+                BorderBrush = Brushes.Black,
+                BorderThickness = new Thickness(2)
+            };
+
+            DownloadPanel = new StackPanel
+            {
+                Width = width * .95,
+                Height = 90,
+            };
+
+            DownloadTextBox = new TextBox
+            {
+                Width = width * 0.9,
+                Height = 30,
+                Margin = new Thickness(5, 10, 0, 0),
+            };
+
+            DownloadProgressBar = new ProgressBar
+            {
+                Width = DownloadTextBox.Width * .9,
+                Height = 30,
+                Margin = new Thickness(10)
+            };
+            DownloadProgressBar.Visibility = Visibility.Collapsed;
+
+            CancelButton = new Button
+            {
+                Margin = new Thickness(10, 10, 10, 10),
+                Width = 100,
+                Height = 30,
+                HorizontalAlignment = System.Windows.HorizontalAlignment.Right,
+                Content = "Cancel",
+                Background = Brushes.DodgerBlue,
+                Foreground = Brushes.White,
+                Visibility = Visibility.Collapsed
+            };
+
+            DownloadButton = new Button
+            {
+                Margin = new Thickness(10, 10, 10, 10),
+                Width = 100,
+                Height = 30,
+                HorizontalAlignment = System.Windows.HorizontalAlignment.Right,
+                Content = "Download",
+                Background = Brushes.DodgerBlue,
+                Foreground = Brushes.White,
+            };
+
+            DownloadBorder.Child = DownloadPanel;
+
+            DownloadPanel.Children.Add(DownloadTextBox);
+            DownloadPanel.Children.Add(DownloadButton);
+            DownloadPanel.Children.Add(DownloadProgressBar);
+            DownloadPanel.Children.Add(CancelButton);
+        }
+
+        private void ShowDownloadStuff()
+        {
+            CancelButton.Visibility = Visibility.Collapsed;
+            DownloadProgressBar.Visibility = Visibility.Collapsed;
+            DownloadButton.Visibility = Visibility.Visible;
+            DownloadTextBox.Visibility = Visibility.Visible;
+        }
+
+        private void HideDownloadStuff()
+        {
+            CancelButton.Visibility = Visibility.Visible;
+            DownloadProgressBar.Visibility = Visibility.Visible;
+            DownloadButton.Visibility = Visibility.Collapsed;
+            DownloadTextBox.Visibility = Visibility.Collapsed;
+        }
+
+        public async Task DownloadAsync()
+        {
+            DownloadProgressBar.Value = 0;
+
+            using (var dlg = new System.Windows.Forms.SaveFileDialog())
+            {
+                var dialogResult = dlg.ShowDialog();
+                if (dialogResult == DialogResult.OK)
+                {
+                    var filePath = dlg.FileName;
+                    if (DownloadTextBox.Text != string.Empty &&
+                        Uri.IsWellFormedUriString(DownloadTextBox.Text, UriKind.RelativeOrAbsolute))
+                    {
+                        using (var client = new HttpClient())
+                        {
+                            //client.DownloadProgressChanged += (send, args) =>
+                            //{
+                            //    progressBar.Value = args.ProgressPercentage;
+                            //};
+                            TokenSource = new CancellationTokenSource();
+
+                            TokenSource.Token.Register(() =>
+                            {
+                                client.CancelPendingRequests();
+
+                                ShowDownloadStuff();
+                            });
+
+                            HideDownloadStuff();
+
+                            try
+                            {
+                                HttpResponseMessage response = await client.GetAsync(DownloadTextBox.Text);
+                                response.EnsureSuccessStatusCode();
+
+                                using (var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None))
+                                {
+                                    await response.Content.CopyToAsync(fileStream);
+                                }
+
+                            }
+                            catch (HttpRequestException exception)
+                            {
+                                MessageBox.Show(exception.Message);
+                            }
+                            catch (TaskCanceledException)
+                            {
+                                MessageBox.Show("Downloading was canceled");
+                            }
+                            catch (Exception)
+                            {
+                                throw;
+                            }
+                            finally
+                            {
+                                ShowDownloadStuff();
+
+                                TokenSource.Dispose();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid uri, please enter valid one");
+                    }
+                }
+            }
+        }
+    }
+}
+
--- FileDownloader.WPF/MainWindow.xaml.cs
+++ FileDownloader.WPF/MainWindow.xaml.cs
@@ -1,19 +1,7 @@
-﻿using System;
+﻿using FileDownloader.Services;
 using System.Collections.Generic;
-using System.IO;
-using System.Net;
-using System.Net.Http;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Forms;
-using System.Windows.Media;
 using Button = System.Windows.Controls.Button;
-using MessageBox = System.Windows.MessageBox;
-using ProgressBar = System.Windows.Controls.ProgressBar;
-using TextBox = System.Windows.Controls.TextBox;
 namespace FileDownloader.WPF
 {
     /// <summary>
@@ -21,194 +9,42 @@ namespace FileDownloader.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        Dictionary<Button, (TextBox, ProgressBar, Button)> downloadControls;
-        Dictionary<Button, CancellationTokenSource> tokens;
+        Dictionary<Button, DownloadListBoxItem> downloadControls;
+        Dictionary<Button, Button> buttons;
 
         public MainWindow()
         {
             InitializeComponent();
-            downloadControls = new Dictionary<Button, (TextBox, ProgressBar, Button)>();
+            downloadControls = new Dictionary<Button, DownloadListBoxItem>();
 
-            tokens = new Dictionary<Button, CancellationTokenSource>();
-        }
-
-        private async Task DownloadAsync(Button button)
-        {
-            if (downloadControls.ContainsKey(button))
-            {
-                var textBox = downloadControls[button].Item1;
-                var progressBar = downloadControls[button].Item2;
-                var cancelButton = downloadControls[button].Item3;
-
-                progressBar.Value = 0;
-
-                using (var dlg = new SaveFileDialog())
-                {
-                    var dialogResult = dlg.ShowDialog();
-                    if (dialogResult == System.Windows.Forms.DialogResult.OK)
-                    {
-                        var filePath = dlg.FileName;
-                        if (textBox.Text != string.Empty && Uri.IsWellFormedUriString(textBox.Text, UriKind.RelativeOrAbsolute))
-                        {
-                            byte[] buffer;
-                            using (var client = new HttpClient())
-                            {
-                                //client.DownloadProgressChanged += (send, args) =>
-                                //{
-                                //    progressBar.Value = args.ProgressPercentage;
-                                //};
-                                var token = new CancellationTokenSource();
-                                tokens[cancelButton] = token;
-                                token.Token.Register(() =>
-                                {
-                                    cancelButton.Visibility = Visibility.Collapsed;
-                                    progressBar.Visibility = Visibility.Collapsed;
-                                    button.Visibility = Visibility.Visible;
-                                    textBox.Visibility = Visibility.Visible;
-
-                                    //client.CancelAsync();
-                                    client.Dispose();
-
-                                    ////////////////////////
-                                    //GC.Collect();
-                                    //GC.WaitForPendingFinalizers();
-                                    ///////////////////////
-
-                                    var file = new FileInfo(filePath);
-                                    file.Attributes = file.Attributes & ~FileAttributes.ReadOnly;
-
-                                    try
-                                    {
-                                        file.Delete();
-                                    }
-                                    catch (IOException ex)
-                                    {
-                                        MessageBox.Show(ex.Message);
-                                    }
-                           
-                                    //File.Delete(filePath);
-                                });
-
-                                cancelButton.Visibility = Visibility.Visible;
-                                progressBar.Visibility = Visibility.Visible;
-                                button.Visibility = Visibility.Collapsed;
-                                textBox.Visibility = Visibility.Collapsed;
-
-                                try
-                                {
-                                    var stream = await client.GetStreamAsync(textBox.Text);
-                                    buffer = new byte[stream.Length];
-                                    await stream.ReadAsync(buffer, 0, (int)stream.Length);
-
-                                    using (var s = new StreamWriter(filePath, false, Encoding.UTF8, buffer.Length))
-                                    {
-                                        s.Write(buffer);
-                                    }
-
-                                    stream.Dispose();
-                                }
-                                catch (WebException exception)
-                                {
-                                    MessageBox.Show(exception.Message);
-                                }
-                                catch (Exception)
-                                {
-                                    throw;
-                                }
-                                finally
-                                {
-                                    cancelButton.Visibility = Visibility.Collapsed;
-                                    progressBar.Visibility = Visibility.Collapsed;
-                                    button.Visibility = Visibility.Visible;
-                                    textBox.Visibility = Visibility.Visible;
-
-                                    token.Dispose();
-                                }
-                            }
-                        }
-                        else
-                        {
-                            MessageBox.Show("Invalid uri, please enter valid one");
-                        }
-                    }
-                }
-            }
+            buttons = new Dictionary<Button, Button>();
         }
 
         private void AddNewDownloadButton_Click(object sender, RoutedEventArgs e)
         {
-            var border = new Border
-            {
-                BorderBrush = Brushes.Black,
-                BorderThickness = new Thickness(2)
-            };
+            DownloadListBoxItem item = new DownloadListBoxItem((int)DownloadsListBox.Width);
+            item.CancelButton.Click += CancelButton_Click;
+            item.DownloadButton.Click += Download_ClickAsync;
 
-            var newDownload = new StackPanel
-            {
-                Width = DownloadsListBox.Width * .95,
-                Height = 90,
-            };
-            border.Child = newDownload;
+            DownloadsListBox.Items.Add(item.DownloadBorder);
 
-            var textBox = new System.Windows.Controls.TextBox
-            {
-                Width = DownloadsListBox.Width * 0.9,
-                Height = 30,
-                Margin = new Thickness(5, 10, 0, 0),
-            };
-            newDownload.Children.Add(textBox);
-
-            var progressBar = new System.Windows.Controls.ProgressBar
-            {
-                Width = textBox.Width * .9,
-                Height = 30,
-                Margin = new Thickness(10)
-            };
-            progressBar.Visibility = Visibility.Collapsed;
-            newDownload.Children.Add(progressBar);
-
-            var cancelButton = new Button
-            {
-                Margin = new Thickness(10, 10, 10, 10),
-                Width = 100,
-                Height = 30,
-                HorizontalAlignment = System.Windows.HorizontalAlignment.Right,
-                Content = "Cancel",
-                Background = Brushes.DodgerBlue,
-                Foreground = Brushes.White,
-                Visibility = Visibility.Collapsed
-            };
-            cancelButton.Click += CancelButton_Click;
-            newDownload.Children.Add(cancelButton);
-
-            var button = new Button
-            {
-                Margin = new Thickness(10, 10, 10, 10),
-                Width = 100,
-                Height = 30,
-                HorizontalAlignment = System.Windows.HorizontalAlignment.Right,
-                Content = "Download",
-                Background = Brushes.DodgerBlue,
-                Foreground = Brushes.White,
-            };
-            button.Click += Download_ClickAsync;
-            newDownload.Children.Add(button);
-
-            DownloadsListBox.Items.Add(border);
-            downloadControls.Add(button, (textBox, progressBar, cancelButton));
+            downloadControls.Add(item.DownloadButton, item);
+            buttons.Add(item.CancelButton, item.DownloadButton);
         }
 
         private void CancelButton_Click(object sender, RoutedEventArgs e)
         {
             var cancelButton = (Button)sender;
-            tokens[cancelButton].Cancel();
-            tokens[cancelButton].Dispose();
+            var downloadButton = buttons[cancelButton];
+            downloadControls[downloadButton].TokenSource.Cancel();
+            downloadControls[downloadButton].TokenSource.Dispose();
         }
 
         private async void Download_ClickAsync(object sender, RoutedEventArgs e)
         {
-            var button = ((Button)sender);
-            await DownloadAsync(button);
+            var downloadButton = ((Button)sender);
+
+            await downloadControls[downloadButton].DownloadAsync();
         }
     }
 }
